d6c93cf3fe0a4eb460665f935a0eab5096ca65ca
Zonas Sun <346410840@163.com>
2016-12-06 11:49:21 +0800

    Add CheckDomain.ps1

---
+++ CheckDomain.ps1
@@ -0,0 +1,17 @@
+function Get-DomainInfo
+{
+	param([Parameter(Mandatory)]$Domain)
+	process
+	{
+		$user = Get-Credential;
+		$bstr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($user.Password)
+		$p = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($bstr)
+		$domainPath = "LDAP://"+ $Domain;
+		echo $domainPath;
+		$d = new-object System.DirectoryServices.DirectoryEntry($domainPath, $user.Username, $p);
+		echo $d;
+	}
+	
+}
+
+Get-DomainInfo
