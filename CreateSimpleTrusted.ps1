b7152c87aa5a37d91490099ff5faaa1f38afb41e
Zonas Sun <346410840@163.com>
2017-01-05 14:50:19 +0800

    Update CreateSimpleTrusted.ps1

--- CreateSimpleTrusted.ps1
+++ CreateSimpleTrusted.ps1
@@ -7,12 +7,12 @@ function CreateSimple
         [Parameter(Mandatory)]$ProviderName)
 	process
 	{
-    $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($CertificatePath);
-    $map1 = New-SPClaimTypeMapping $IdentityClaimType -IncomingClaimTypeDisplayName "Name ID" -SameAsIncoming
-    $realm = $ReplyPartyIdentifier
-    $signinurl = $SignInUrl
-    $ap = New-SPTrustedIdentityTokenIssuer -Name $ProviderName -Description "ADFS 3.0 Federated Server" -Realm $realm -ImportTrustCertificate $cert -ClaimsMappings $map1 -SignInUrl $signinurl -IdentifierClaim $map1.InputClaimType
-  }
+    		$cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($CertificatePath);
+   		$map1 = New-SPClaimTypeMapping $IdentityClaimType -IncomingClaimTypeDisplayName "Name ID" -SameAsIncoming
+    		$realm = $ReplyPartyIdentifier
+    		$signinurl = $SignInUrl
+    		$ap = New-SPTrustedIdentityTokenIssuer -Name $ProviderName -Description "ADFS 3.0 Federated Server" -Realm $realm -ImportTrustCertificate $cert -ClaimsMappings $map1 -SignInUrl $signinurl -IdentifierClaim $map1.InputClaimType
+  	}
 	
 }
 
